262fc366c35e77f8646e906706718c1c81ed56d9
Clément Uster <clement.uster@gmail.com>
2023-03-16 18:03:21 +0100

    Implement generate address

---
+++ go.mod
@@ -0,0 +1,43 @@
+module github.com/archethic-foundation/archethic-cli
+
+go 1.20
+
+require (
+	github.com/archethic-foundation/libgo v0.0.0-unpublished
+	github.com/charmbracelet/bubbles v0.13.0
+	github.com/charmbracelet/bubbletea v0.22.1
+	github.com/charmbracelet/lipgloss v0.5.0
+)
+
+require (
+	github.com/aead/ecdh v0.2.0 // indirect
+	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/decred/dcrd/dcrec/edwards/v2 v2.0.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1 v1.0.3 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hasura/go-graphql-client v0.9.1 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.12.0 // indirect
+	github.com/nshafer/phx v0.2.0 // indirect
+	github.com/rivo/uniseg v0.3.4 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
+
+// TODO: needs to be removed when package is published
+replace github.com/archethic-foundation/libgo v0.0.0-unpublished => ../libgo
---
+++ tui/mainui/model.go
@@ -0,0 +1,79 @@
+package mainui
+
+import (
+	"github.com/archethic-foundation/archethic-cli/tui/constants"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// SelectMenuMsg the message to change the view to the selected entry
+type SelectMsg struct {
+	ActiveMenu uint
+}
+
+// Model the Menu model definition
+type Model struct {
+	list list.Model
+}
+
+// New initialize the projectui model for your program
+func New() tea.Model {
+	m := Model{list: list.NewModel(menu(), list.NewDefaultDelegate(), 100, 25)} //Updated the default width and height to large defaults from 0.
+	m.list.Title = "Archethic CLI"
+	return m
+}
+
+// Init run any intial IO on program start
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+// Update handle IO and commands
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m = m.UpdateWindowSize(msg.Width, msg.Height).(Model)
+	case tea.KeyMsg:
+		switch {
+		case msg.String() == "ctrl+c":
+			return m, tea.Quit
+		case key.Matches(msg, constants.Keymap.Enter):
+			cmd = selectCmd(uint(m.list.Cursor()))
+		default:
+			m.list, cmd = m.list.Update(msg)
+		}
+		cmds = append(cmds, cmd)
+	}
+	return m, tea.Batch(cmds...)
+}
+
+// View return the text UI to be output to the terminal
+func (m Model) View() string {
+	return constants.DocStyle.Render(m.list.View() + "\n")
+}
+
+func (m Model) UpdateWindowSize(w int, h int) tea.Model {
+	top, right, bottom, left := constants.DocStyle.GetMargin()
+	m.list.SetSize(w-left-right, h-top-bottom-1)
+	return m
+}
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+func menu() []list.Item {
+	menu := []list.Item{
+		item{title: "Generate Address", desc: ""},
+		item{title: "Build & Send Transaction (TODO)", desc: ""},
+		item{title: "Keychain Management (TODO)", desc: ""},
+	}
+	return menu
+}
