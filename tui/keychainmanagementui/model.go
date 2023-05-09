9eeb0f942b8f61617f1846455188db245e1e03a4
Clément Uster <clement.uster@gmail.com>
2023-05-09 19:14:17 +0200

    avoid creating a keychain that already exists

--- tui/keychainmanagementui/model.go
+++ tui/keychainmanagementui/model.go
@@ -269,6 +269,11 @@ func createKeychain(m *Model) error {
 	keychainTx.OriginSign(originPrivateKey)
 
 	client := archethic.NewAPIClient(url)
+	accessKeychain, _ := archethic.GetKeychain(accessSeed, *client)
+	if accessKeychain != nil {
+		return errors.New("keychain access already exists")
+	}
+
 	ts := archethic.NewTransactionSender(client)
 	ts.AddOnRequiredConfirmation(func(nbConf int) {
 		m.feedback += "\nKeychain's transaction confirmed."
